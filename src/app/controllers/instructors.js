88a43ed23dab82c849567b1eb45b8430469b3492
joaopa24 <joaokn129@gmail.com>
2020-12-04 20:27:56 -0300

    04/12 20:27 models

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -23,6 +23,7 @@ module.exports = {
         Instructor.create(req.body, function(instructor){
             return res.redirect(`/instructors/${instructor.id}`)
         })
+         
     },
     show(req, res) {
         return
