0962f265a530bda030b52e12044758cfad32bd11
joaopa24 <joaokn129@gmail.com>
2020-12-30 19:33:09 -0300

    paginate algorithm added

--- public/script.js
+++ public/script.js
@@ -6,3 +6,36 @@ for(item of menuItems){
           item.classList.add("active")
      }
 }
+
+// Paginação
+// totalPages = 20 
+// selectedPage = 15
+// [1, ..., 13, 14, 15, 16, 17, ..., 20]
+
+let totalPages = 20,
+    selectedPage = 6,
+    pages = [],
+    oldPage
+
+for(let currentPage = 1; currentPage <= totalPages; currentPage++){
+     
+     const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+     const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+     const pagesBeforeSelectedPage = currentPage >= selectedPage - 2 
+
+     if(firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage){
+        
+        if(oldPage && currentPage - oldPage > 2){
+             pages.push("...")
+        }
+        if(oldPage && currentPage - oldPage == 2){
+             pages.push(currentPage - 1)
+        }
+        
+        pages.push(currentPage)
+        oldPage = currentPage
+     }
+
+}
+
+console.log(pages)
--- src/app/models/instructor.js
+++ src/app/models/instructor.js
@@ -1,3 +1,4 @@
+const { off } = require('../../config/db')
 const db = require('../../config/db')
 const { age, date } = require("../../lib/utils")
 
@@ -93,6 +94,32 @@ module.exports = {
             if (err) throw `Database Error ${err}`
 
             return callback()
+        })
+    },
+    paginate(params){
+        const { filter , limit, offset , callback} = params
+        
+        let query = `
+        SELECT instructors.*, count(members) as total_students
+        FROM instructors
+        LEFT JOIN members ON (instructors.id = members.instructor_id)
+        `
+
+        if( filter ){
+            query = `${query}
+            WHERE instructors.name ILIKE '%${filter}%'
+            OR instructors.services ILIKE '%${filter}%'
+            `
+        }
+
+        query = `${query}
+        GROUP BY instructors.id LIMIT $1 OFFSET $2
+        
+        ` 
+        db.query(query, [limit , offset], function(err,results){
+            if(err) throw 'Database Error'
+
+            callback(results.rows)
         })
     }
 }
